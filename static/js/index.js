be516efce82678d12547ec81f869ceb63befda6c
XiChao <nicexch@126.com>
2021-02-24 21:04:41 +0800

    :pencil2: Fix typos.

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,10 @@
-; (function (doc) {
+; (function(doc) {
   var action = sessionStorage.getItem('action');
-  console.log(action)
-  if(!action) {
-    doc.getElementsByClassName('J_cat')[0].classList.add('action_cat')
-    doc.getElementsByClassName('J_toGit')[0].classList.add('action_target')
-    sessionStorage.setItem('action', 1)
+
+  if (!action) {
+    doc.getElementsByClassName('J_cat')[0].classList.add('action_cat');
+    doc.getElementsByClassName('J_toGit')[0].classList.add('action_target');
+    sessionStorage.setItem('action', 1);
   }
+
 })(document);
